8cc7c53b4ee96a034bffdfc78ee6c1fda6b0264a
pharsfalvi <peter@qtc.hu>
2015-03-09 09:34:13 +0100

    Project structure added

---
+++ src/test/java/com/gembaboo/aptz/AirportTimezoneServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.gembaboo.aptz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AirportTimezoneServiceApplication.class)
+public class AirportTimezoneServiceApplicationTests {
+
+
+	@Test
+	public void applicationStarts(){
+		AirportTimezoneServiceApplication.main(new String[0]);
+	}
+
+}
