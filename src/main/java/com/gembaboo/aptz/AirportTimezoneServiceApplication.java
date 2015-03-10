b885009c0026fa224711add16f735f0da0cc6a94
Peter Harsfalvi <peter.harsfalvi@lhsystems.com>
2015-03-10 19:03:16 +0100

    Added docker support and ssh access

--- src/main/java/com/gembaboo/aptz/AirportTimezoneServiceApplication.java
+++ src/main/java/com/gembaboo/aptz/AirportTimezoneServiceApplication.java
@@ -10,4 +10,6 @@ public class AirportTimezoneServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AirportTimezoneServiceApplication.class, args);
     }
+
+
 }
